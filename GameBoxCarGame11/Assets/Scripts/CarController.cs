1ffb189ee914946f2c38a254643809b9614d2dac
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2020-11-15 16:32:27 +0300

    делаю управление машинкой

---
+++ GameBoxCarGame11/Assets/Scripts/CarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] float speed = 100;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(speed * Vector3.forward * Time.deltaTime);
+        }
+    }
+}
