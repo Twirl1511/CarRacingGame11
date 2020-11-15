b318fe1836bd1ece9114d82c9ce907519491ee6f
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2020-11-15 20:25:18 +0300

    Новый проект

---
+++ GameBox_11/Assets/Scripts/CarController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    
+    [SerializeField] float speed = 10f;
+    [SerializeField] float torqueForce = 10f;
+    [SerializeField] float driftFactor = 0.5f;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Accelerate"))
+        {
+            Debug.Log("нажата Accelerate");
+            //rb.AddRelativeForce(Vector2.up * speed);
+            rb.AddForce(transform.up * speed);
+        }
+        if (Input.GetButton("Brakes"))
+        {
+            Debug.Log("нажата Brakes");
+            rb.AddForce(transform.up * -speed/5);
+        }
+        rb.AddTorque(Input.GetAxis("Left") * torqueForce);
+
+        rb.velocity = Vector2.Dot(rb.velocity, transform.up) * transform.up;
+    }
+
+    private Vector2 RightVelocity()
+    {
+        return transform.right * Vector2.Dot(rb.velocity, transform.right);
+    }
+    private Vector2 ForwardVelocity()
+    {
+        return  transform.up * Vector2.Dot(rb.velocity, transform.up);
+    }
+
+   
+}
