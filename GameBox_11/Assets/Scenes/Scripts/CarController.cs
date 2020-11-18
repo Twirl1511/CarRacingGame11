b3ade3dec8b239c192b971c26c37d4ffa1ba1f09
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2020-11-18 23:23:49 +0300

    добавил параметр контроля поворота при нужной скорости

--- GameBox_11/Assets/Scenes/Scripts/CarController.cs
+++ GameBox_11/Assets/Scenes/Scripts/CarController.cs
@@ -18,10 +18,13 @@ public class CarController : MonoBehaviour
     [SerializeField] private float Player1_DriftFactor = 0.93f;
     [SerializeField] private float Player2_DriftFactor = 0.93f;
 
+    [SerializeField] private float Player1_Speed_for_torgue = 3f;
+    [SerializeField] private float Player2_Speed_for_torgue = 3f;
+
+
 
     void FixedUpdate()
     {
-
         #region [Player_1]
 
         if (Input.GetButton("P1_forward"))
@@ -35,7 +38,14 @@ public class CarController : MonoBehaviour
             Player1_Rigidbody.AddForce(Player1_Transform.up * -Player1_Speed / Player1_SlowDown);
         }
 
-        Player1_Rigidbody.AddTorque(Input.GetAxis("P1_horizontal") * Player1_SorgueForce);
+        Debug.Log(Player1_Rigidbody.velocity.magnitude);
+        if (Player1_Rigidbody.velocity.magnitude > Player1_Speed_for_torgue)
+        {
+            Player1_Rigidbody.AddTorque(Input.GetAxis("P1_horizontal") * Player1_SorgueForce);
+        }
+            
+        
+        
 
         Player1_Rigidbody.velocity = ForwardVelocity(Player1_Transform, Player1_Rigidbody) + RightVelocity(Player1_Transform, Player1_Rigidbody) * Player1_DriftFactor;
 
@@ -54,9 +64,11 @@ public class CarController : MonoBehaviour
             Player2_Rigidbody.AddForce(Player2_Transform.up * -Player2_Speed / Player2_SlowDown);
         }
 
-        
-        // math.lerp
-        Player2_Rigidbody.AddTorque(Input.GetAxis("P2_horizontal") * Player2_TorqueForce);
+
+        if (Player2_Rigidbody.velocity.magnitude > Player2_Speed_for_torgue)
+        {
+            Player2_Rigidbody.AddTorque(Input.GetAxis("P2_horizontal") * Player2_TorqueForce);
+        }
 
         Player2_Rigidbody.velocity = ForwardVelocity(Player2_Transform, Player2_Rigidbody) + RightVelocity(Player2_Transform, Player2_Rigidbody) * Player1_DriftFactor;
 
