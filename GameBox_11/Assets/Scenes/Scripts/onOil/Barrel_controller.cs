f9bbbef2424655df13b0d19f332902d24f92dbd4
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2020-11-22 13:08:42 +0300

    канистры появляются корректно, убрах хардкод

--- GameBox_11/Assets/Scenes/Scripts/onOil/Barrel_controller.cs
+++ GameBox_11/Assets/Scenes/Scripts/onOil/Barrel_controller.cs
@@ -8,12 +8,39 @@ public class Barrel_controller : MonoBehaviour
     [SerializeField] private GameObject BarrelPrefab;
     private bool BarrelDelayFlag = true;
     private const int MAX_BARRELS_ON_MAP = 2;
-    public int CurrentBarrelsOnMap = 2;
+    [HideInInspector] public int CurrentBarrelsOnMap = 2;
+
+    [SerializeField] private Transform TopLeftOuterCorner;
+    [SerializeField] private Transform TopRightOuterCorner;
+    [SerializeField] private Transform DownLeftOuterCorner;
+
+    [SerializeField] private Transform TopLeftInnerCorner;
+    [SerializeField] private Transform TopRightInnerCorner;
+    [SerializeField] private Transform DownLeftInnerCorner;
+
+    private float LeftOuterX;
+    private float RightOuterX;
+    private float TopOuterY;
+    private float DownOuterY;
+
+    private float LeftInnerX;
+    private float RightInnerX;
+    private float TopInnerY;
+    private float DownInnerY;
+
 
 
     private void Start()
     {
-        
+        LeftOuterX = TopLeftOuterCorner.position.x; // -766
+        RightOuterX = TopRightOuterCorner.position.x; // 842
+        TopOuterY = TopLeftOuterCorner.position.y; //371
+        DownOuterY = DownLeftOuterCorner.position.y; // -346
+
+        LeftInnerX = TopLeftInnerCorner.position.x; // -611
+        RightInnerX = TopRightInnerCorner.position.x; // 691
+        TopInnerY = TopLeftInnerCorner.position.y; // 213
+        DownInnerY = DownLeftInnerCorner.position.y; // -191
     }
     private void FixedUpdate()
     {
@@ -28,14 +55,15 @@ public class Barrel_controller : MonoBehaviour
     // убрать хардкод когда появится карта, брать координаты боксов границ минус 1 или 2 юнита
     private Vector3 FindPosition()
     {
+
         float x;
         float y;
         do
         {
-            x = Random.Range(-53, 38);
-            y = Random.Range(-22, 30);
+            x = Random.Range(LeftOuterX, RightOuterX);
+            y = Random.Range(DownOuterY, TopOuterY);
         }
-        while ((x > -45 && x < 31.7) && (y > -9.5 && y < 18.7));
+        while ((x > LeftInnerX && x < RightInnerX) && (y > DownInnerY && y < TopInnerY));
         return new Vector3(x,y,0);
     }
 
