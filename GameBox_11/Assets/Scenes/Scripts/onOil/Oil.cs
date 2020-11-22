60c5e87c2c10db38931ee2f804eeb041b8475cf7
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2020-11-22 23:31:53 +0300

    пофиксил баг с лужой

--- GameBox_11/Assets/Scenes/Scripts/onOil/Oil.cs
+++ GameBox_11/Assets/Scenes/Scripts/onOil/Oil.cs
@@ -11,6 +11,12 @@ public class Oil : MonoBehaviour
     private float DefaultLinerDragFactor;
     public bool Flag = true;
     [SerializeField] private float TimeToDisappear = 2;
+    private enum PlayerStates
+    {
+        stayOnOil,
+        cleanedFromOil
+    }
+    private PlayerStates State = PlayerStates.cleanedFromOil;
 
     private void Awake()
     {
@@ -18,45 +24,32 @@ public class Oil : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Flag)
+        if (State.Equals(PlayerStates.cleanedFromOil))
         {
             DefaultAngularDragFactor = collision.gameObject.GetComponent<Rigidbody2D>().angularDrag;
             DefaultLinerDragFactor = collision.gameObject.GetComponent<Rigidbody2D>().drag;
-            Flag = false;
         }
-        
+        collision.gameObject.GetComponent<Rigidbody2D>().angularDrag = AngularDragFactor;
+        collision.gameObject.GetComponent<Rigidbody2D>().drag = LinerDragFactor;
+        State = PlayerStates.stayOnOil;
+        StartCoroutine(SetDefaultForPlayer(collision));
     }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            collision.gameObject.GetComponent<Rigidbody2D>().angularDrag = AngularDragFactor;
-            collision.gameObject.GetComponent<Rigidbody2D>().drag = LinerDragFactor;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            StartCoroutine(SetDefaultForPlayer(collision));
-        }
-    }
-
 
     private IEnumerator SetDefaultForPlayer(Collider2D collision)
     {
         yield return new WaitForSeconds(TimeToCleanUpFromOil);
         collision.gameObject.GetComponent<Rigidbody2D>().angularDrag = DefaultAngularDragFactor;
         collision.gameObject.GetComponent<Rigidbody2D>().drag = DefaultLinerDragFactor;
-        Flag = true;
+        State = PlayerStates.cleanedFromOil;
+        
     }
 
     private IEnumerator DisappearOil()
     {
         yield return new WaitForSeconds(TimeToDisappear);
+        Destroy(gameObject.GetComponent<Collider2D>());
+        yield return new WaitForSeconds(3);
         Destroy(gameObject);
-
     }
 
 
