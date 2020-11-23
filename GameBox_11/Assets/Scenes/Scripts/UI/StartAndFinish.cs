96bf89811186d5a0688517ec5e53e92eb2b083ef
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2020-11-23 16:08:29 +0300

    добавил звук отсчета, смену музыки и включение управления только после отсчета

---
+++ GameBox_11/Assets/Scenes/Scripts/UI/StartAndFinish.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAndFinish : MonoBehaviour
+{
+    [SerializeField] private AudioSource CountdownSound;
+    [SerializeField] private AudioSource GameSound;
+    [SerializeField] private AudioSource MenuSound;
+
+    [SerializeField] private GameObject Player1_Car;
+    [SerializeField] private GameObject Player1_Moto;
+    [SerializeField] private GameObject Player2_Car;
+    [SerializeField] private GameObject Player2_Moto;
+
+    public void OnClickStartButton()
+    {
+        MenuSound.Stop();
+        CountdownSound.Play();
+        StartCoroutine(StartTheGame());
+
+    }
+    private IEnumerator StartTheGame()
+    {
+        yield return new WaitForSeconds(CountdownSound.clip.length);
+        Player1_Car.GetComponent<Player_Controller>().enabled = true;
+        Player1_Moto.GetComponent<Player_Controller>().enabled = true;
+        Player2_Car.GetComponent<Player_Controller>().enabled = true;
+        Player2_Moto.GetComponent<Player_Controller>().enabled = true;
+        GameSound.Play();
+    }
+
+
+}
