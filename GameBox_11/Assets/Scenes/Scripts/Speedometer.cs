f412b5cf5b505744c5e3e7f50039bc4295572be8
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2020-11-30 18:44:28 +0300

    спидометр в работе

--- GameBox_11/Assets/Scenes/Scripts/Speedometer.cs
+++ GameBox_11/Assets/Scenes/Scripts/Speedometer.cs
@@ -4,22 +4,48 @@ using UnityEngine;
 
 public class Speedometer : MonoBehaviour
 {
-    [SerializeField] private GameObject Arrow;
+    [SerializeField] private GameObject Player1_ArrowBlue;
     [SerializeField] private GameObject Player1_CarBlue;
     [SerializeField] private GameObject Player1_MotoBlue;
     [SerializeField] private GameObject Player2_CarRed;
     [SerializeField] private GameObject Player2_MotoRed;
-    private GameObject Player1_Blue;
-    private GameObject Player2_Red;
+    private float Player1_BlueSpeed;
+    private float Player2_RedSpeed;
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ShowSpeed();
+        BlueArrowMove();
     }
+
+    private void ShowSpeed()
+    {
+        if (Player1_CarBlue.activeSelf)
+        {
+            Player1_BlueSpeed = Player1_CarBlue.GetComponent<Rigidbody2D>().velocity.magnitude;
+        }
+        if (Player1_MotoBlue.activeSelf)
+        {
+            Player1_BlueSpeed = Player1_MotoBlue.GetComponent<Rigidbody2D>().velocity.magnitude;
+        }
+        if (Player2_CarRed.activeSelf)
+        {
+            Player2_RedSpeed = Player2_CarRed.GetComponent<Rigidbody2D>().velocity.magnitude;
+        }
+        if (Player2_MotoRed.activeSelf)
+        {
+            Player2_RedSpeed = Player2_MotoRed.GetComponent<Rigidbody2D>().velocity.magnitude;
+        }
+    }
+    private void BlueArrowMove()
+    {
+        
+        Player1_ArrowBlue.transform.Rotate(Vector3.back * Player1_BlueSpeed * 0.001f);
+    }
+
 }
