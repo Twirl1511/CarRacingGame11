94e94948927c7ae5f47b61bad8adc3fe1d7c00ed
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2020-12-04 16:09:38 +0300

    что-то похожее на дым...

---
+++ GameBox_11/Assets/Scenes/Scripts/OnPlayer/Smoke.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smoke : MonoBehaviour
+{
+    [SerializeField] private GameObject SmokeObj;
+    [SerializeField] private GameObject SmokePosition;
+    [SerializeField] private Buttons PlayerForwardButton;
+    [SerializeField] private ParticleSystem SmokeParticalSystem;
+    private enum Buttons
+    {
+        P1_forward,
+        P2_forward
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown(PlayerForwardButton.ToString()))
+        {
+            SmokeParticalSystem.Play();
+        }
+        if (Input.GetButtonUp(PlayerForwardButton.ToString()))
+        {
+            SmokeParticalSystem.Stop();
+        }
+    }
+    void Update()
+    {
+        SmokeObj.transform.position = new Vector2(SmokePosition.transform.position.x, SmokePosition.transform.position.y);
+        SmokeObj.transform.rotation = Quaternion.Euler(transform.rotation.x,
+            transform.rotation.y,
+            transform.rotation.z);
+    }
+    
+}
