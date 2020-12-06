311c1770f9030e41d6242775b6d880ac9e1af2a6
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2020-12-06 16:04:39 +0300

    кнопки переключения работают, осталось настроить для второго плеера

---
+++ GameBox_11/Assets/Scenes/Scripts/UI/MainMenu/SwitchRacer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchRacer : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Player1_RacersBlue;
+    [SerializeField] private Button SwitchLeftPlayer1_Blue;
+    [SerializeField] private Button SwitchRightPlayer1_Blue;
+
+    [SerializeField] private GameObject[] Player2_RacersRed;
+    [SerializeField] private Button SwitchLeftPlayer2_Red;
+    [SerializeField] private Button SwitchRightPlayer2_Red;
+
+    private int Player1_BlueRacerNumber = 0;
+    private int Player2_RedRacerNumber = 0;
+
+    public void OnLeftSwitchClickPlayer1_Blue()
+    {
+        Debug.Log("LEFT");
+        Player1_BlueRacerNumber--;
+        if (Player1_BlueRacerNumber < 0) Player1_BlueRacerNumber = 2;
+        for (int i = 0; i < Player1_RacersBlue.Length; i++)
+        {
+            Player1_RacersBlue[i].SetActive(false);
+        }
+        Player1_RacersBlue[Player1_BlueRacerNumber].SetActive(true);
+    }
+
+    public void OnRightSwitchClickPlayer1_Blue()
+    {
+        Debug.Log("RIGHT");
+        Player1_BlueRacerNumber++;
+        if (Player1_BlueRacerNumber > 2) Player1_BlueRacerNumber = 0;
+        for (int i = 0; i < Player1_RacersBlue.Length; i++)
+        {
+            Player1_RacersBlue[i].SetActive(false);
+        }
+        Player1_RacersBlue[Player1_BlueRacerNumber].SetActive(true);
+    }
+
+    public void OnLeftSwitchClickPlayer2_Red()
+    {
+
+    }
+
+    public void OnRightSwitchClickPlayer2_Red()
+    {
+
+    }
+
+
+}
