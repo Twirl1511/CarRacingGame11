29b54b05be3847236db0bdc0a630839a3b4fded5
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2020-12-06 12:20:52 +0300

    добавил тестовую сцену

---
+++ GameBox_11/Assets/Scenes/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject PlayerToFollow;
+    void Update()
+    {
+        Vector3 v3 = new Vector3(PlayerToFollow.GetComponent<Transform>().position.x, PlayerToFollow.GetComponent<Transform>().position.y, transform.position.z);
+        transform.position = v3;
+    }
+}
