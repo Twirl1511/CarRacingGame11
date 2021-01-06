f1c570ea72525214fc4f9cbf9c15140492ae9f6b
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-01-06 19:13:53 +0300

    запрет на одинаковые машины, мелкие правки, анимация синего трака

--- GameBox_11/Assets/Scenes/Scripts/UI/MainMenu/SwitchRacer.cs
+++ GameBox_11/Assets/Scenes/Scripts/UI/MainMenu/SwitchRacer.cs
@@ -14,12 +14,44 @@ public class SwitchRacer : MonoBehaviour
     [SerializeField] private Button SwitchRightPlayer2_Red;
 
     private int Player1_BlueRacerNumber = 0;
-    private int Player2_RedRacerNumber = 0;
+    private int Player2_RedRacerNumber = 1;
+    private void Start()
+    {
+        for (int i = 0; i < Player1_RacersBlue.Length; i++)
+        {
+            Player1_RacersBlue[i].SetActive(false);
+        }
+        Player1_RacersBlue[Player1_BlueRacerNumber].SetActive(true);
+        for (int i = 0; i < Player2_RacersRed.Length; i++)
+        {
+            Player2_RacersRed[i].SetActive(false);
+        }
+        Player2_RacersRed[Player2_RedRacerNumber].SetActive(true);
+    }
 
+    private void Update()
+    {
+        
+    }
+    private bool SameRacers()
+    {
+        if (Player1_BlueRacerNumber == Player2_RedRacerNumber)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
     public void OnLeftSwitchClickPlayer1_Blue()
     {
-        Debug.Log("LEFT");
         Player1_BlueRacerNumber--;
+        if (Player1_BlueRacerNumber < 0) Player1_BlueRacerNumber = 2;
+        while (SameRacers())
+        {
+            Player1_BlueRacerNumber--;
+            if (Player1_BlueRacerNumber < 0) Player1_BlueRacerNumber = 2;
+        }
         if (Player1_BlueRacerNumber < 0) Player1_BlueRacerNumber = 2;
         for (int i = 0; i < Player1_RacersBlue.Length; i++)
         {
@@ -30,9 +62,13 @@ public class SwitchRacer : MonoBehaviour
 
     public void OnRightSwitchClickPlayer1_Blue()
     {
-        Debug.Log("RIGHT");
         Player1_BlueRacerNumber++;
         if (Player1_BlueRacerNumber > 2) Player1_BlueRacerNumber = 0;
+        while (SameRacers())
+        {
+            Player1_BlueRacerNumber++;
+            if (Player1_BlueRacerNumber >2) Player1_BlueRacerNumber = 0;
+        }
         for (int i = 0; i < Player1_RacersBlue.Length; i++)
         {
             Player1_RacersBlue[i].SetActive(false);
@@ -43,6 +79,12 @@ public class SwitchRacer : MonoBehaviour
     public void OnLeftSwitchClickPlayer2_Red()
     {
         Player2_RedRacerNumber--;
+        if (Player2_RedRacerNumber < 0) Player2_RedRacerNumber = 2;
+        while (SameRacers())
+        {
+            Player2_RedRacerNumber--;
+            if (Player2_RedRacerNumber < 0) Player2_RedRacerNumber = 2;
+        }
         if (Player2_RedRacerNumber < 0) Player2_RedRacerNumber = 2;
         for (int i = 0; i < Player2_RacersRed.Length; i++)
         {
@@ -55,6 +97,11 @@ public class SwitchRacer : MonoBehaviour
     {
         Player2_RedRacerNumber++;
         if (Player2_RedRacerNumber > 2) Player2_RedRacerNumber = 0;
+        while (SameRacers())
+        {
+            Player2_RedRacerNumber++;
+            if (Player2_RedRacerNumber > 2) Player2_RedRacerNumber = 0;
+        }
         for (int i = 0; i < Player2_RacersRed.Length; i++)
         {
             Player2_RacersRed[i].SetActive(false);
