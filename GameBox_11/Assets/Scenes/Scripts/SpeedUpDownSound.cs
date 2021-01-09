56b8d69d7b89c0d4f7c9516fa4f1e5e26c3e1598
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-01-09 13:15:49 +0300

    звуки набора скорости и торможения

---
+++ GameBox_11/Assets/Scenes/Scripts/SpeedUpDownSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUpDownSound : MonoBehaviour
+{
+    [SerializeField] private AudioSource SpeedUpSound;
+    [SerializeField] private AudioSource SpeedDownSound;
+
+    private string _speedUpButton;
+    private void Start()
+    {
+        _speedUpButton = gameObject.GetComponent<Player_Controller>().PlayerForwardButton.ToString();
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetButton(_speedUpButton))
+        {
+            if (SpeedUpSound.isPlaying)
+            {
+                return;
+            }
+            Player_Controller.PlaySound(gameObject, SpeedUpSound);
+        }
+
+        if (Input.GetButtonUp(_speedUpButton))
+        {
+            if (SpeedDownSound.isPlaying)
+            {
+                return;
+            }
+            Player_Controller.PlaySound(gameObject, SpeedDownSound);
+        }
+    }
+}
--- GameBox_11/Assets/WheelCollisionSound.cs
+++ GameBox_11/Assets/WheelCollisionSound.cs
@@ -17,4 +17,5 @@ public class WheelCollisionSound : MonoBehaviour
         }
         
     }
+
 }
